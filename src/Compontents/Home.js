b69ec2b140eb268308dc4a3e73d69d131dfd4e45
saravanan642 <saravanansenthil605@gmail.com>
2026-01-02 20:55:56 +0530

    update

--- src/Compontents/Home.js
+++ src/Compontents/Home.js
@@ -9,11 +9,28 @@ const Home = () => {
 
       <form>
         <p>Job list</p>
-       <input
-  className="border border-black border-solid p-2 rounded"
-  type="number"
-  placeholder="eg. 56839005"
-/>
+        <div className=' flex  gap-[40px] bg-slate-600 ml-[2%]  w- fit c'>
+          <div className=' flex gap-[20px] m-[10px]'>
+            <label className=' text-black font-bold'>Job code</label>
+            <input className="border-2 border-solid border-black rounded" type="number " placeholder="eg. 56839005" />
+          </div>
+
+          <div className=' flex gap-[20px] m-[10px]'>
+            <label className=' text-black font-bold'>Job Tittle</label>
+            <input className="border-2 border-solid border-black rounded " type="text " placeholder="eg. Fullstack developer " />
+          </div>
+
+
+          <div className=' flex gap-[20px] m-[10px]'>
+            <label className=' text-black font-bold'>Client code Id </label>
+            <input className="border-2 border-solid border-black rounded " type="number" placeholder="eg. 56839005" />
+          </div>
+        </div>
+
+
+
+
+
 
       </form>
     </div>
